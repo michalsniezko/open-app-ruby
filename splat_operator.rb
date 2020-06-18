71364cc1962b5ee9c162956eaef44d14f25f764b
Michał Śnieżko <michalsniezko@gmail.com>
2020-06-18 14:16:07 +0200

    splat operator lesson

--- splat_operator.rb
+++ splat_operator.rb
@@ -4,3 +4,12 @@ def say_hello(first, last, *options)
 end
 
 say_hello("Mike", "Śnieżko", "Something")
+
+arr = ["something", "some other thing"]
+say_hello(*arr) #unpacking array
+
+
+hash = {a: 1, b: 2}
+hash2 = {**hash, c: 3}
+
+p hash2
